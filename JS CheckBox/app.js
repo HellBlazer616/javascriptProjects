8e2adf2b374ba10c35c46fb0f24bcb9f93b168d2
Rifat Hossain <rifat.hossain.dhrubo@gmail.com>
2020-03-03 11:35:39 +0600

    js checkbox

---
+++ JS CheckBox/app.js
@@ -0,0 +1,23 @@
+const checkBoxes = document.querySelectorAll('input[type=checkbox]');
+
+let lastChecked;
+
+function handleCheck(e) {
+	let inBetween = false;
+	if (e.shiftKey && this.checked) {
+		checkBoxes.forEach((checkBox) => {
+			if (checkBox === this || checkBox === lastChecked) {
+				inBetween = !inBetween;
+			}
+			if (inBetween) {
+				checkBox.checked = true;
+			}
+		});
+	}
+
+	lastChecked = this;
+}
+
+checkBoxes.forEach((checkBox) => {
+	checkBox.addEventListener('click', handleCheck);
+});
